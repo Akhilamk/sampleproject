38239f595d41f31de7c81a365511038990ef0cad
Akhilamk <mkakhi1992@gmail.com>
2021-04-22 07:52:31 -0700

    first comit

--- resources/views/userlogin.blade.php
+++ resources/views/userlogin.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login here</title>
-    <link href="{{ asset('style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('style.css')}}">
 </head>
-<body >
+<body>
 
 <form action="" method="get" align ="center">
 <table cellspacing="10px" cellpadding="10px" align="center" width="200px"> 
