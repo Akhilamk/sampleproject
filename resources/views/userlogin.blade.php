1a1b53fe3fb2c4ba4ab53dea0ec76c1cf3a11f7f
Akhilamk <mkakhi1992@gmail.com>
2021-04-22 04:25:26 -0700

    first commit

---
+++ resources/views/userlogin.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login here</title>
+    <link rel="stylesheet" href="{{url('style.css')}}">
+</head>
+<body >
+
+<form action="" method="get" align ="center">
+<table cellspacing="10px" cellpadding="10px" align="center" width="200px"> 
+			<tr>
+				<td><u> USER LOGIN<br><br><img src="{{url('images/log.jpg')}}" ></u></td>
+					
+			</tr>
+			<tr>
+				<td><input type="text" placeholder="UserName"></td>
+			</tr>
+			<tr>
+				<td><input type="password" placeholder="Password"></td>
+			</tr>
+			<tr>
+				<td><button>LOGIN</button></td>
+			</tr>
+			
+		</table>
+		
+	</form>
+    
+</body>
+</html>
